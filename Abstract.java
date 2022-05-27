14bab8c19cf511b1eacd4df309b8118ed494fb5e
Hrushikesh More <rushi.more1904@gmail.com>
2022-05-27 17:11:17 +0530

    Abstract.java created

---
+++ Abstract.java
@@ -0,0 +1,26 @@
+abstract class A{
+	abstract void m1();
+	abstract void m2();
+}
+abstract class B extends A{
+	@Override
+	void m1(){
+		System.out.println("m1 solution");
+	}
+	
+}
+ class C extends B{
+	@Override
+	void m2(){
+		System.out.println("m2 solution");
+	}
+	
+}
+class Abstract{
+	public static void main(String... args){
+		A ob=new C();
+		ob.m1();
+		ob.m2();
+		
+	}
+}
